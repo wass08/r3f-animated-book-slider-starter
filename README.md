38939d7bbce0b040e7635e268a37ebd7a376534b
wass08 <wass08@gmail.com>
2024-07-27 08:49:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# r3f-skibidi-starter
+
+![YouTube Thumbnail](https://github.com/wass08/r3f-skibidi-starter/assets/6551176/d5501917-6d32-46c6-b41b-2ae7da0a47b0)
+
+[Video tutorial](https://youtu.be/ljL7NKCi-kg)
+
+[Live demo](https://r3f-skibidi-final.vercel.app/)
+
+[Final code](https://github.com/wass08/r3f-skibidi-final)
---
+++ src/components/Experience.jsx
@@ -0,0 +1,21 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <Environment preset="studio"></Environment>
+      <directionalLight
+        position={[2, 5, 2]}
+        intensity={2.5}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-bias={-0.0001}
+      />
+      <mesh position-y={-1.5} rotation-x={-Math.PI / 2} receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+};
